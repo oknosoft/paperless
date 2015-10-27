ff367ca33f1576cd56bd8645f60853c6ebf78e29
unpete <info@oknosoft.ru>
2015-10-27 23:28:57 +0500

    регистрация по штрихкоду

--- js/reg.js
+++ js/reg.js
@@ -28,7 +28,7 @@ $p.settings = function (prm, modifiers) {
 	prm.data_url = "data/";
 
 	// расположение файла инициализации базы sql
-	//prm.create_tables = "data/create_tables.sql";
+	prm.create_tables = "data/create_tables.sql";
 
 	// полноэкранный режим на мобильных устройствах
 	prm.request_full_screen = true;
@@ -48,6 +48,105 @@ $p.settings = function (prm, modifiers) {
 	// разрешаем покидать страницу без лишних вопросов
 	$p.eve.redirect = true;
 
+	// подключаем модификатор, в котором реализуем отложенную запись штрихкодов
+	modifiers.push(function($p){
+
+		var _mgr = $p.doc.barcodes, // менеджер документа штрихкодов
+			store_name = "barcodes";
+
+		function connect_idx_store(name){
+
+			return new Promise(function(resolve, reject){
+				var request = indexedDB.open(name, 1);
+				request.onerror = function(err){
+					reject(err);
+				};
+				request.onsuccess = function(){
+					// При успешном открытии вызвали коллбэк передав ему объект БД
+					resolve(request.result);
+				};
+				request.onupgradeneeded = function(e){
+					// Если БД еще не существует, то создаем хранилище объектов.
+					e.currentTarget.result.createObjectStore(name, { keyPath: "ref" });
+					return connect_idx_store(name);
+				}
+			});
+		}
+
+		connect_idx_store(store_name)
+			.then(function (idx_store) {
+
+				function save_idxdb(o){
+					var request = idx_store.transaction([store_name], "readwrite").objectStore(store_name).put(o._obj);
+					request.onerror = function(err){
+						console.log(err);
+					};
+					request.onsuccess = function(){
+						return request.result;
+					}
+				}
+
+				// попытка синхронизации всех незасинхронизированных
+				function try_sync(){
+
+				}
+
+
+				function restore_idxdb(){
+
+					var barcodes = [];
+
+					idx_store.transaction([store_name], "readonly").objectStore(store_name).openCursor().onsuccess = function(event) {
+						var cursor = event.target.result;
+						if (cursor) {
+							barcodes.push(cursor.value);
+							cursor.continue();
+						}
+						else {
+							if(barcodes.length)
+								_mgr.load_array(barcodes);
+
+						}
+					};
+				}
+
+				// такт синхронизации запускаем раз в минуту
+				setInterval(try_sync, 60000);
+
+				/**
+				 * Обработчик события "перед записью"
+				 * @this {DataObj} - обработчик вызывается в контексте текущего объекта
+				 * @return {Boolean} - если ложь, стандартные операции не выполняются
+				 */
+				_mgr.attache_event("before_save", function (attr) {
+
+					// запишем в indexeddb, чтобы восстановить после перезапуска браузера
+					save_idxdb(this);
+
+					return false;
+				});
+
+				_mgr.__define({
+					clear_idxdb: {
+						value: function (date) {
+
+						}
+					}
+				});
+
+				// читаем кеш
+				restore_idxdb();
+
+			})
+			.catch(function (err) {
+				console.log(err);
+			});
+
+
+
+	});
+
+
 };
 
 /**
@@ -55,47 +154,179 @@ $p.settings = function (prm, modifiers) {
  */
 $p.iface.oninit = function() {
 
-	var myTabbar = new dhtmlXTabBar({
+	var hprm,       // параметры URL
+		tabmgrs = { // менеджеры закладок
 
-		parent:             document.body,    // id/object, container for tabbar
+			scan: function(cell){
 
-		arrows_mode:        "auto",         // mode of showing tabs arrows (auto, always)
+				if($p.iface._scan)
+					return;
 
+				$p.iface._scan = {
+					layout: cell.attachLayout({
+						pattern: "2E",
+						cells: [
+							{id: "a", text: "search", height: 34, header: false, fix_size: [false, true]},
+							{id: "b", text: "grid", header: false}
+						],
+						offsets: {
+							top: 0,
+							right: 0,
+							bottom: 0,
+							left: 0
+						}
+					})
+				};
+				$p.iface._scan.input = $p.iface._scan.layout.cells("a");
+				$p.iface._scan.input.setMinHeight(34);
+				$p.iface._scan.input.setHeight(34);
+				$p.iface._scan.input.cell.lastChild.style.paddingTop = "4px";
+				$p.iface._scan.input.cell.lastChild.style.paddingBottom = "12px";
+
+				// драйвер штрихкода
+				$p.iface.scandrv = new $p.iface.ScanDriver($p.iface._scan.input);
+
+				// обработчик события штрихкода
+				$p.iface.scandrv.onscan.push(function (code) {
+					if(code.length > 14){
+						$p.iface.beep.error();
+						return;
+					}
+
+					$p.doc.barcodes.create()
+						.then(function (o) {
+							o.date = new Date();
+							o.number_doc = code;
+							o.save();
+						});
+
+					$p.iface.beep.ok();
+				});
+
+				// список документов штрихкод
+				$p.iface._scan.grid = $p.iface._scan.layout.cells("b");
+				setTimeout(function () {
+					$p.doc.barcodes.form_selection($p.iface._scan.grid, {hide_header: true});
+				}, 100);
+			},
+
+			orders: function(cell){
+
+			},
+
+			report: function(cell){
+
+			},
+
+			settings: function(cell){
+
+	}
+
+		}; // менеджеры закладок
+
+	// midi пикалка - инфонмирует об успешном и ошибочном сканировании
+	$p.iface.beep = {
+		ok: function () {
+			new Beep.Voice( '4D' )
+				.setOscillatorType( 'square' ) //  For that chunky 8-bit sound.
+				.setAttackGain( 0.8 )           //  0 = No gain. 1 = Full gain.
+				.setAttackDuration( 0.1 )      //  Attack ramp up duration in seconds.
+				.setDecayDuration( 0.1 )       //  Decay ramp down duration in seconds.
+				.setSustainGain( 0.5 )          //  Sustain gain level; percent of attackGain.
+				.setSustainDuration( 0.1 )     //  Sustain duration in seconds -- normally Infinity.
+				.setReleaseDuration( 0.1 )     //  Release ramp down duration in seconds.
+				.play( 1.5 );
+		},
+		error: function () {
+			var voice = new Beep.Voice( '1C');
+			function play(){
+				voice.setOscillatorType( 'square' ) //  For that chunky 8-bit sound.
+					.setAttackGain( 0.8 )           //  0 = No gain. 1 = Full gain.
+					.setAttackDuration( 0.05 )      //  Attack ramp up duration in seconds.
+					.setDecayDuration( 0.05 )       //  Decay ramp down duration in seconds.
+					.setSustainGain( 0.5 )          //  Sustain gain level; percent of attackGain.
+					.setSustainDuration( 0.07 )     //  Sustain duration in seconds -- normally Infinity.
+					.setReleaseDuration( 0.07 )     //  Release ramp down duration in seconds.
+					.play( 4 );
+			}
+			for(var count = 0; count < 3; count++)
+				setTimeout(play, count * 250);
+
+		}
+	};
+
+	// основа интерфейса - панель закладок
+	$p.iface.main = new dhtmlXTabBar({
+		parent:             document.body,
+		arrows_mode:        "auto",
 		offsets: {
 			top: 0,
 			right: 0,
 			bottom: 0,
 			left: 0
 		},
-
 		tabs: [
 			{
 				id:      "scan",
-				text:    "Скан",
-				active:  true
+				text:    "Скан"
 			},
 			{
 				id:      "orders",
-				text:    "Заказы",
-				active:  true
+				text:    "Заказы"
 			},
 			{
 				id:      "report",
-				text:    "Отчет",
-				active:  true
+				text:    "Отчет"
 			},
 			{
 				id:      "settings",
-				text:    "Настройки",
-				active:  true
+				text:    "Настройки"
 			}
 		]
+	});
+	$p.iface.docs = $p.iface.main.cells("report");
+
+	$p.iface.main.attachEvent("onSelect", function(id){
+
+		hprm = $p.job_prm.parse_url();
+		if(hprm.view != id)
+			$p.iface.set_hash(hprm.obj, hprm.ref, hprm.frm, id);
+
+		setTimeout(function () {
+			tabmgrs[id]($p.iface.main.cells(id))
+		});
+
+		return true;
 
 	});
+
+	// маршрутизация URL
+	$p.eve.hash_route.push(function (hprm) {
+
+		// view отвечает за переключение закладки в TabBar
+		if(hprm.view && $p.iface.main.getActiveTab() != hprm.view){
+			$p.iface.main.getAllTabs().forEach(function(item){
+				if(item == hprm.view)
+					$p.iface.main.tabs(item).setActive();
+			});
+		}
+
+		return false;
+	});
+
+	$p.eve.auto_log_in()
+		.then(function () {
+			hprm = $p.job_prm.parse_url();
+			if(!hprm.view || $p.iface.main.getAllTabs().indexOf(hprm.view) == -1)
+				$p.iface.set_hash(hprm.obj, hprm.ref, hprm.frm, "scan");
+			else
+				setTimeout($p.iface.hash_route);
+		})
+		.catch(function (err) {
+			console.log(err);
+		});
+
+
 };
 
-$p.eve.hash_route.push(function (hprm) {
-
-	return false;
-});
 
