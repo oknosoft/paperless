b09abe96e8403898d17fa9f50959d11e0477dfd4
unpete <info@oknosoft.ru>
2015-11-23 01:40:30 +0500

    регистрация по штрихкоду

--- js/worker.js
+++ js/worker.js
@@ -9,7 +9,7 @@
 
 self.addEventListener("message", function(e) {
 
-	importScripts("../lib_ext/metadata.core.js");
+	importScripts("../lib/metadata.core.js");
 
 	if(e.data.action == "lazy_load"){
 		var attr = e.data;
