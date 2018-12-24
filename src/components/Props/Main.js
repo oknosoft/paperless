9d66101f0bccb0df1a492d3c71bbef68bdcb16bc
unpete <info@oknosoft.ru>
2018-12-24 16:06:55 +0500

    Вывод параметров изделия и фурнитуры

--- src/components/Props/Main.js
+++ src/components/Props/Main.js
@@ -5,7 +5,6 @@ import Typography from '@material-ui/core/Typography';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
 import TableRow from './TableRow';
 import Params from './Params';
 
--- src/components/Props/Params.js
+++ src/components/Props/Params.js
@@ -7,16 +7,16 @@ import TableRow from './TableRow';
 export default function Params(props) {
 
   const {ox: {params}, cnstr} = props;
-
   const res = [];
 
-  params.find_rows({cnstr}, (row) => {
-    row.param && row.param.set && res.push(<TableRow key={row.param}>
-      <TableCell component="th" scope="row">row.param.name</TableCell>
-      <TableCell>{row.value}</TableCell>
-    </TableRow>)
+  params.find_rows({cnstr, inset: $p.utils.blank.guid}, (row) => {
+    row.param && row.param.include_to_description && res.push(row);
   });
 
-
-  return res;
+  return res
+    .sort((a, b) => a.param.sorting_field - b.param.sorting_field)
+    .map(({param, value}) => <TableRow key={param.ref}>
+      <TableCell component="th" scope="row">{param.name}</TableCell>
+      <TableCell>{value ? value.toString() : ''}</TableCell>
+    </TableRow>);
 }
