ec43e2f5cda50634e7c1a60a8f611f02a1ecae43
unpete <info@oknosoft.ru>
2019-12-25 20:52:28 +0500

    рабочее место арки-трапеции

--- src/components/Arc/Arc.js
+++ src/components/Arc/Arc.js
@@ -1,23 +1,71 @@
 import React from 'react';
-import Helmet from 'react-helmet';
-import Typography from '@material-ui/core/Typography';
-import AppContent from 'metadata-react/App/AppContent';
-import {item_props} from '../App/menu';
-import withStyles, {WorkPlace} from '../App/WorkPlace';
+import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import {withIface} from 'metadata-redux';
+import Builder from '../Builder';
+import Props from '../Props/Main';
+import Profiles from './Profiles';
+import withStyles, {WorkPlace, WorkPlaceFrame} from '../App/WorkPlace';
 
 class Arc extends WorkPlace {
 
+  barcodeFin(bar) {
+    const {state: {full_picture}, editor: {project}} = this;
+    const {cnstr, ox} = bar;
+    project.load(ox, {auto_lines: full_picture, custom_lines: full_picture, mosquito: full_picture})
+      .then(() => {
+        if(full_picture) {
+          return;
+        }
+
+        const contour = project.getItem({cnstr});
+        if(contour) {
+          // рисуем текущий слой
+          project.draw_fragment({elm: -cnstr});
+
+          // прячем заполнения
+          contour.glasses(true);
+
+          // рисуем спецразмеры импостов
+          contour.l_dimensions.draw_by_imposts();
+
+          // подкрашиваем штульпы
+          this.editor.color_shtulps(contour);
+
+          // показываем номера элементов на палках
+          for(const profile of contour.profiles) {
+            if(!profile.elm_type._manager.impost_lay.includes(profile.elm_type)) {
+              profile.show_number();
+            }
+          }
+          contour.l_visualization._by_spec.opacity = 0.4;
+
+          // вписываем в размер экрана
+          project.zoom_fit();
+          this.setState(bar);
+        }
+      });
+  }
+
   render() {
-    const iprops = item_props();
-    return <AppContent >
-      <Helmet title={iprops.text}>
-        <meta name="description" content={iprops.title} />
-      </Helmet>
-      <div style={{marginTop: 16}}>
-        <Typography variant="h4" component="h1" color="primary">{iprops.title}</Typography>
-      </div>
-    </AppContent>;
+    const {state: {ox, cnstr, full_picture}, props: {classes}, editor} = this;
+    const contour = !full_picture && editor && ox && ox.empty && !ox.empty() && editor.project.getItem({cnstr});
+    return <WorkPlaceFrame>
+      <Grid item sm={12} lg={full_picture ? 9 : 7} className={classes.workplace}>
+        <Builder registerChild={this.registerEditor}/>
+      </Grid>
+      <Grid item sm={12} lg={full_picture ? 3 : 5} className={classes.props}>
+        <Props ox={ox} cnstr={0} show_spec={false} changeFull={this.changeFull}/>
+        {contour && <Profiles {...this.state} contour={contour}/>}
+      </Grid>
+    </WorkPlaceFrame>;
   }
 }
 
-export default withStyles(Arc);
+Arc.propTypes = {
+  handleIfaceState: PropTypes.func.isRequired,
+  title: PropTypes.string,
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(withIface(Arc));
---
+++ src/components/Arc/Profiles.js
@@ -0,0 +1,77 @@
+/**
+ * Профили текущего контура
+ *
+ * @module Profiles
+ *
+ * Created by Evgeniy Malyarov on 20.12.2019.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import TabularSection from 'metadata-react/TabularSection';
+import Typography from '@material-ui/core/Typography';
+
+class Profiles extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+    const {cat, utils} = $p;
+    cat.scheme_settings.find_rows({obj: 'cat.characteristics.coordinates'}, (scheme) => {
+      if(scheme.name.endsWith('arc')) {
+        this.scheme = scheme;
+        scheme.filter = this.filter.bind(this);
+
+        this.ox = cat.characteristics.get();
+        this._meta = utils._clone(this.ox._metadata('coordinates'));
+        this._meta.fields.len.type.fraction = 0;
+        this._meta.fields.alp1.type.fraction = 1;
+        this._meta.fields.alp2.type.fraction = 1;
+        this._meta.fields.r.type.fraction = 0;
+      }
+    });
+  }
+
+  filter(collection) {
+    const res = [];
+    collection.clear();
+    const {cnstr, ox: {coordinates, specification}, contour: {profiles}} = this.props;
+    const {elm_types} = $p.enm;
+    coordinates.forEach((row) => {
+      if(profiles.some((profile) => profile.elm === row.elm)) {
+        const nrow = collection.add(row);
+        const srow = specification.find({elm: row.elm, nom: row.nom.ref});
+        if(srow) {
+          nrow.len = srow.len * 1000;
+        }
+        res.push(nrow);
+      }
+    });
+    return res;
+  }
+
+  render() {
+    return this.scheme ?
+      <div style={{maxHeight: 600, minHeight: 400}}>
+        <TabularSection
+          _obj={this.ox}
+          _meta={this._meta}
+          _tabular="coordinates"
+          scheme={this.scheme}
+          read_only
+          hideToolbar
+        />
+      </div>
+      :
+      <Typography key="err-nom" color="error">
+        {`Не найден элемент scheme_settings {obj: "cat.characteristics.coordinates", name: "characteristics.coordinates.welding"}`}
+      </Typography>;
+  }
+
+}
+
+Profiles.propTypes = {
+  ox: PropTypes.object.isRequired,
+  cnstr: PropTypes.number.isRequired,
+};
+
+export default Profiles;
