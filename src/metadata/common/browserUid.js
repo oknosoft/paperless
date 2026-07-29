065e5a8cb87ca6a8c59b94dfcf3d5a946c7bfba3
unpete <info@oknosoft.ru>
2026-07-29 18:51:45 +0300

    рабочий центр из url при запуске

--- src/metadata/common/browserUid.js
+++ src/metadata/common/browserUid.js
@@ -1,15 +1,17 @@
 
-export function browserUid({wsql}){
+export function browserUid({wsql, adapters, cat}){
 
   const urlid = new URLSearchParams(location.search.substring(1));
   if(urlid.has('id')) {
     wsql.set_user_param('url_id', urlid.get('id'));
   }
-  // const signal = AbortSignal.timeout(2000);
-  // fetch(`http://localhost:9190/uid`, {signal})
-  //   .then((res) => res.text())
-  //   .then((urlid) => {
-  //     wsql.set_user_param('browser_uid', urlid);
-  //   })
-  //   .catch(() => null);
+  if(urlid.has('wc')) {
+    const id = urlid.get('wc');
+    adapters.pouch.once('pouch_complete_loaded', () => {
+      const wc = cat.work_centers.by_id(id);
+      if(wc && !wc.empty()) {
+        wsql.set_user_param('work_center', wc.ref);
+      }
+    });
+  }
 }
